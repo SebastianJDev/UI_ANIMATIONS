2dca866acd58455c15552a17bd9cdbc32cfe5523
SebastianDev <104598507+SebastianJDev@users.noreply.github.com>
2023-07-13 18:46:28 -0500

    Update ScaleTween.cs

--- ScaleTween.cs
+++ ScaleTween.cs
@@ -4,39 +4,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
-public class ScaleTween : MonoBehaviour
+namespace Root
 {
-    public LeanTweenType inType;
-    public LeanTweenType outType;
-    public float duration;
-    public float delay;
-    public UnityEvent onCompleteCallback;
+    public class ScaleTween : MonoBehaviour
+    {
+        public LeanTweenType inType;
+        public LeanTweenType outType;
+        public float duration;
+        public float delay;
+        public UnityEvent onCompleteCallback;
+        public Vector3 escala = new Vector3(1f,1f,1f);
 
-    public void OnEnable()
-    {
-        transform.localScale = new Vector3(0, 0, 0);
-        LeanTween.scale(gameObject, new Vector3(2, 2, 2), duration).setDelay(delay).setEase(inType).setOnComplete(OnComplete);
-    }
-    public void OnComplete()
-    {
-        if(onCompleteCallback != null)
+        public void OnEnable()
         {
-            onCompleteCallback.Invoke();
+            transform.localScale = new Vector3(0, 0, 0);
+            LeanTween.scale(gameObject, new Vector3(escala.x, escala.y, escala.z), duration).setDelay(delay).setEase(inType).setOnComplete(OnComplete);
         }
-    }
+        public void OnComplete()
+        {
+            if(onCompleteCallback != null)
+            {
+                onCompleteCallback.Invoke();
+            }
+        }
 
 
 
-    public void OnClose()
-    {
-        //LeanTween.scale(gameObject,new Vector3(0,0,0), 0.5f).setOnComplete(DestroyMe);
-        LeanTween.moveX(gameObject, -1500, duration).setDelay(delay).setEase(outType).setOnComplete(DestroyMe);
-    }
-    void DestroyMe()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        Destroy(gameObject);
-        
-
+        public void OnClose()
+        {
+            //LeanTween.scale(gameObject,new Vector3(0,0,0), 0.5f).setOnComplete(DestroyMe);
+            LeanTween.moveX(gameObject, -1500, duration).setDelay(delay).setEase(outType).setOnComplete(DestroyMe);
+        }
+        void DestroyMe()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Destroy(gameObject);
+        }
     }
 }
